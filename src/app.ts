179c0c51790802f6ddf435d1e13e71c1d5df5ebb
Al Joseph <condinoaljoseph@gmail.com>
2021-03-13 14:34:26 +0800

    updated port

--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,10 @@ import { validateBody, createUrlString } from './util';
 import { getResponse } from './carbon';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded());
-
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('dist/app.js'));
-}
 
 app.get('/', function (req: Request, res: Response) {
 	res.send('snipp it carbon api');
