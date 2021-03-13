6ca47b8d45ab4e2c927e95a31610428f6e204052
Al Joseph <condinoaljoseph@gmail.com>
2021-03-13 13:31:22 +0800

    fixed

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,10 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded());
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('dist/app.js'));
+}
+
 app.get('/', function (req: Request, res: Response) {
 	res.send('snipp it carbon api');
 });
